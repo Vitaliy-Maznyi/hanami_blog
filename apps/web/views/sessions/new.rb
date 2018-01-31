dac4dd861ff48e2ef4deb25b69c5c1393d474590
VitaliyMaznyi <vitalii.maznyi@fasterthanlight.me>
2018-01-31 12:40:28 +0200

    add styles for sign in form
    fix navbar auth buttons, improve navbar mobile

--- apps/web/views/sessions/new.rb
+++ apps/web/views/sessions/new.rb
@@ -2,12 +2,5 @@ module Web::Views::Sessions
   class New
     include Web::View
 
-    def form
-      form_for :session, routes.sessions_path do
-        text_field :email
-        password_field :password
-        submit 'Sign in'
-      end
-    end
   end
 end
