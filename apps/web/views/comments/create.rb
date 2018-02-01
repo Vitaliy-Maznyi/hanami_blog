bac226a0cc3348db5bb0e388da8e5ec749b8cec9
VitaliyMaznyi <vitalii.maznyi@fasterthanlight.me>
2018-02-01 18:42:07 +0200

    add comment authorization, add fa icons, styles for remove comment cross

--- apps/web/views/comments/create.rb
+++
@@ -1,5 +0,0 @@
-module Web::Views::Comments
-  class Create
-    include Web::View
-  end
-end
--- apps/web/views/posts/show.rb
+++ apps/web/views/posts/show.rb
@@ -3,7 +3,11 @@ module Web::Views::Posts
     include Web::View
 
     def current_user_author?
-      current_user.id == post.user.id
+      user_signed_in? && current_user.id == post.user.id
+    end
+
+    def current_user_commenter?(comment)
+      user_signed_in? && current_user.id == comment.user.id
     end
   end
 end
